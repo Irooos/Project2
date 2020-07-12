e1644f0b95d2e3a64bb4abcec91a76d5c39f4501
Shannon McCutcheon <scm0495@gmail.com>
2020-07-12 15:19:33 -0400

    js
    Still trying to solve some js errors. I put one of the test codes in as a note for now.

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,6 +1,6 @@
-function getData(){
+/***function getData(){
 var input = $("#searchtext").val()
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=lmkpW3GGTV7iiw3UXzPZqvqSrWIq3s5G=30");
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6zaT0xFJmzC=5");
 xhr.done(function(response) { 
 
     console.log("success got data", response); 
@@ -12,5 +12,33 @@ xhr.done(function(response) {
         $('.inner').append("'<img src='"+jiffs[i].images.original.url+"' style='height:350px; width:350px;'/>")
     }
         });
-    }
+    }***/
+
+
+
+
+    /***var api = "http://api.giphy.com/v1/gifs/search";
+    var apiKey = "&api_key=dc6zaT0xFJmzC";
+    var_query = "&q=rainbow"
+
+    $.ajax({
+        url: 'https://api.giphy.com/v1/gifs/search' + input + '&api_key=dc6zaT0xFJmzC', 
+        dataType: 'jsonp',
+        type: 'GET',
+        data: {access_api: api, count: var_query},
+        success: function(data){
+             console.log(data);
+            for( x in data.data ){
+                $('ul').append('<li><img src="'+data[x].images.original.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
+                // data.data[x].images.thumbnail.url - URL of image 150х150
+                // data.data[x].images.standard_resolution.url - URL of image 612х612
+                // data.data[x].link - Instagram post URL 
+            }
+        },
+        error: function(data){
+            console.log(data); // send the error notifications to console
+        }
+    });***/
+    
+    
     
